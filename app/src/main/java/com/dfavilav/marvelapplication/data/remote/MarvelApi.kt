85caed224fd63dc9c8e831aa9f9cbe1b89bb2f93
David Felipe Avila <davilav@unal.edu.co>
2024-02-01 09:10:46 -0500

    feat:Configure Hilt and Create Network DI

--- app/src/main/java/com/dfavilav/marvelapplication/data/remote/MarvelApi.kt
+++ app/src/main/java/com/dfavilav/marvelapplication/data/remote/MarvelApi.kt
@@ -1,6 +1,7 @@
 package com.dfavilav.marvelapplication.data.remote
 
 import com.dfavilav.marvelapplication.domain.model.ApiResponse
+import com.dfavilav.marvelapplication.util.Constants
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
@@ -10,8 +11,8 @@ interface MarvelApi {
     @GET("/v1/public/characters/{character_id}/")
     suspend fun getAllComicsByCharacter(
         @Path("character_id") characterId: Int,
-        @Query("ts") timeStamp: Int = 1000,
-        @Query("apikey") apiKey: String = "4839d517cbd95f77daccd79268143e6d",
-        @Query("hash") hash: String = "296a339c775ab44827539831d4ca46b6",
+        @Query("ts") timeStamp: Int = Constants.TIMESTAMP,
+        @Query("apikey") apiKey: String = Constants.API_KEY,
+        @Query("hash") hash: String = Constants.HASH,
     ): ApiResponse
 }
